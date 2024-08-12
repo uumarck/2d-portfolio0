ca05a0bcae19fe8cea0a1c089087a61c3d475b60
marckanderson <2mandersonb@gmail.com>
2024-08-12 19:18:23 -0300

    assets02

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import { scaleFactor } from "./constants";
 import { k } from "./kaboomCtx";
 
 k.loadSprite("spritesheet", "./spritesheet.png", {
@@ -5,6 +6,64 @@ k.loadSprite("spritesheet", "./spritesheet.png", {
   sliceY:31,
   anims: {
     "idle_down": 936,
+    "walk_down": { from: 936, to :939, loop: true, speed: 8},
+    "idle_side": 975,
+    "walk_side": { from: 975, to :978, loop: true, speed: 8},
+    "idle_up": 1014,
+    "walk_up": { from: 1014, to :1017, loop: true, speed: 8},
   }
 }
 );
+
+k.loadSprite("map", "./map.png");
+
+k.setBackground(k.Color.fromHex("#311047"));
+
+k.scene("main", async () => {
+  const mapData = await (await fetch("./map.json")).json();
+  const layers mapData.layers;
+
+  const map = k.make([k.sprite("map"), k.pos(0), k.scale(scaleFactor)]);
+
+  const player = k.make([
+    k.sprite("spritesheet", {anim: "idle.down" }),
+    k.area({
+      shape: new k.Rect(k.vec2(0, 3), 10, 10);
+    })
+    k.body();
+    k.anchor("center"),
+    k.pos(),
+    k.scale(scaleFactor),
+    {
+      speed: 250,
+      direction: "down",
+      isInDialogue: false,
+    };
+    "player";
+]);
+
+  for (const layer of layers) {
+    if (layer.name === "boundaries") {
+      for (const  boundary of layer.objects) {
+        map.add([
+          k.area([
+            shape: new k.Rect(k.vec2(0), boundary.width, boundary.height),
+          ]),
+          k.body({ isStatic: true }),
+          k.pos(boundary.x, boundary.y),
+          boundary.name,
+        ])
+
+        if (boundary.name) {
+          player.onCollide(boundary.name, () =>{
+            player.isInDialogue = true;
+            // TODO 
+          })
+        }
+      }
+    }
+  }
+});
+
+
+k.go("main");
