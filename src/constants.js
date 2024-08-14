7051e30ef3e0c36c0c69ffe5a927ded4154bc588
marckanderson <2mandersonb@gmail.com>
2024-08-14 18:34:37 -0300

    done

--- src/constants.js
+++ src/constants.js
@@ -3,11 +3,14 @@ export const scaleFactor= 4;
 export const dialogueData = {
   pc: `My PC. I work mostly in JavaScript, Ruby on Rails, C# and Solidity.
     Here is my <a href="https://github.com/uumarck" target="_blank">Github</a>!`,
-  tv: `That's my TV. I've been watching tech youtubers and learning a lot!`,
-  bed: `This where I sleep to recovery my mental energy.`,
-  resume: `This is my Linkedin. <a href="www.linkedin.com/in/marckanderson" target="_blank">Check it out?</a>
+  tv: `That's my TV. I've been watching tech YouTubers and learning a lot!`,
+  bed: `This is where I sleep to recover my mental energy.`,
+  resume: `This is my LinkedIn. <a href="https://www.linkedin.com/in/marckanderson" target="_blank">Check it out?</a>
   Contact me at 2mandersonb@gmail.com if you have any job opportunities!`,
-  projects: `Info about this portfolio : It's made with the Kaboom.js library which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within canvas. Other games <a href="https://uumarck.itch.io" target="_blank">here</a>.`,
+  projects: `Info about this portfolio: It's made with the Kaboom.js library, which is a library for making games in JavaScript.
+  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within the canvas. Other games <a href="https://uumarck.itch.io" target="_blank">here</a>.`,
   exit: `If you want to exit uumarck's portfolio, just close the tab.`,
+  "sofa-table": `aaaaaaaaaaaaaaaaaaaaaa`,
+  library: `asdasdasdasd`,
+  desk: `sdsadsd`,
 };
