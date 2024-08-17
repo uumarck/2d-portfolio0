894697d072bdfac51a48201c7e32d41d62ab6ba4
marckanderson <2mandersonb@gmail.com>
2024-08-16 21:41:42 -0300

    done

--- src/constants.js
+++ src/constants.js
@@ -1,16 +1,16 @@
 export const scaleFactor= 4;
 
 export const dialogueData = {
-  pc: `My PC. I work mostly in JavaScript, Ruby on Rails, C# and Solidity.
-    Here is my <a href="https://github.com/uumarck" target="_blank">Github</a>!`,
-  tv: `That's my TV. I've been watching tech YouTubers and learning a lot!`,
-  bed: `This is where I sleep to recover my mental energy.`,
-  resume: `This is my LinkedIn. <a href="https://www.linkedin.com/in/marckanderson" target="_blank">Check it out?</a>
-  Contact me at 2mandersonb@gmail.com if you have any job opportunities!`,
-  projects: `Info about this portfolio: It's made with the Kaboom.js library, which is a library for making games in JavaScript.
-  Text is rendered with HTML/CSS. So the textbox you're currently reading is not rendered within the canvas. Other games <a href="https://uumarck.itch.io" target="_blank">here</a>.`,
-  exit: `If you want to exit uumarck's portfolio, just close the tab.`,
-  "sofa-table": `aaaaaaaaaaaaaaaaaaaaaa`,
-  library: `asdasdasdasd`,
-  desk: `sdsadsd`,
+  pc: `Meu super PC! Aqui onde eu hackeio a matrix (ou tento). Espia meu <a href="https://github.com/uumarck" target="_blank">GitHub</a>!`,
+  tv: `Maratonas infinitas no YouTube. Como sobreviveria sem?`,
+  bed: `Aqui recarrego a bateria pra mais codigo. Sono = criatividade!`,
+  resume: `Curioso sobre meu trabalho? Espia meu <a href="https://www.linkedin.com/in/marckanderson" target="_blank">LinkedIn</a>. Bora conectar!`,
+  projects: `Voce esta dentro de um portifolio gamificado, feito com Kaboom.js! Fique a vontade, quer ver mais? Corre <a href="https://uumarck.itch.io" target="_blank">aqui</a> e confira meus outros jogos!`,
+  exit: `Quer fugir? Feche a aba, mas vai deixar saudades!`,
+  "sofa-table": `Mesa de centro ou base de operacoes? Depende do dia.`,
+  library: `Mesa de centro ou base de operacoes? Depende do dia.`,
+  desk: `Meu cantinho organizado... mais ou menos! Aqui a magica acontece.`,
+  fridge: `Meu cantinho organizado... mais ou menos! Aqui a magica acontece.`,
+  hole: `novos updates vindooo galeraa`,
+  
 };
